b9156793635ac9b83f93e6dfb14c3dca19cd2532
judge2020 <hunterray55@hotmail.com>
2015-04-02 16:51:45 -0400

    good

--- kickNoAuth.py
+++ kickNoAuth.py
@@ -1,34 +1,35 @@
 from willie.module import *
 from threading import Timer
 
-timeout = {}
 attempt = {}
+
 @willie.module.event('JOIN')
 @willie.module.rule('.*') #needs to be here
 def kickNoAuth(bot, trigger):
     if 'gamesurge' in trigger.hostmask:
-        print 'a-o-k ' + trigger.nick
+        print 'a-o-k ' + trigger.hostmask
+        return
+    elif 'TTSocial' in trigger.hostmask:
+        print 'a-o-k ' + trigger.hostmask
+        return
     else:
-        if not attempt[trigger.hostmask]:
-            threading.Timer(900, expire, trigger.hostmask)
-            addattempt(trigger.hostmask)
-            bot.msg('Chanserv', trigger.sender + ' KICK ' + trigger.nick + ' Registration in this channel is required. Follow these instructions http://pastebin.com/p9WjjTnK to register for Gamesurge. Join back once you have registered! (Join attempt 1/3)')
-        if attempt[trigger.hostmask] == 2:
-            threading.Timer(900, expire, trigger.hostmask)
-            addattempt(trigger.hostmask)
-            bot.msg('Chanserv', trigger.sender + ' KICK ' + trigger.nick + ' Registration in this channel is required. Follow these instructions http://pastebin.com/p9WjjTnK to register for Gamesurge. Join back once you have registered! (Join attempt 2/3)')
-        if attempt[trigger.hostmask] == 3:
-            threading.Timer(1, expire, trigger.hostmask)
-            bot.msg('Chanserv', trigger.sender + ' tb ' + trigger.nick + ' 2h You have reached Join attempt 3/3. You will be temporarily banned for 2 hours.')
+        if trigger.hostmask not in attempt:
+            Timer(900, expire, trigger.hostmask)
+            attempt[trigger.hostmask] = 1
+            bot.msg('Chanserv', trigger.sender + ' KICK ' + trigger.nick + ' Registration is required. Follow these instructions http://pastebin.com/p9WjjTnK to register for Gamesurge. Join back once registered! (Join attempt 1/3')
+        elif attempt[trigger.hostmask] == 0:
+            Timer(900, expire, trigger.hostmask)
+            attempt[trigger.hostmask] = 1
+            bot.msg('Chanserv', trigger.sender + ' KICK ' + trigger.nick + ' Registration is required. Follow these instructions http://pastebin.com/p9WjjTnK to register for Gamesurge. Join back once registered! (Join attempt 1/3')
+        elif attempt[trigger.hostmask] == 1:
+            Timer(900, expire, trigger.hostmask)
+            attempt[trigger.hostmask] = 2
+            bot.msg('Chanserv', trigger.sender + ' KICK ' + trigger.nick + ' Registration is required. Follow these instructions http://pastebin.com/p9WjjTnK to register for Gamesurge. Join back once registered! (Join attempt 2/3')
+        elif attempt[trigger.hostmask] == 2:
+            Timer(1, expire, trigger.hostmask)
+            bot.msg('Chanserv', trigger.sender + ' tb ' + trigger.nick + ' 30m You have reached Join attempt 3/3 without being registered. You will be temporarily banned for 30 minutes.')
+        else:
+            bot.msg('The_Judge', '88')
 
-def addattempt(hostmask):
-    if not attempt[hostmask]:
-        attempt[hostmask] = 1
-    if attempt[hostmask] == 3:
-        print "i just can't!"
-    else:
-        attempt[hostmask] +=1
-        
 def expire(hostmask):
-    timeout[hostmask] = False
-    
+    attempt[trigger.hostmask] = 0
